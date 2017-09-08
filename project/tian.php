d61b9c04db4aa5035ee6e8ea12ab04f7846c72d7
冰 <30870101+13966349186@users.noreply.github.com>
2017-09-08 15:54:32 +0800

    Add files via upload

---
+++ project/tian.php
@@ -0,0 +1,34 @@
+﻿<?php
+	if(empty($_POST["name"])){
+		exit("充值失败");
+}
+    $host = "http://saweather.market.alicloudapi.com";
+    $path = "/area-to-id";
+    $method = "GET";
+    $appcode = "9fc80b985c154212b3b548de576891c2";
+    $headers = array();
+    array_push($headers, "Authorization:APPCODE " . $appcode);
+    $querys =$_POST["name"];
+	$querys="area=$querys";
+    $bodys = "";
+    $url = $host . $path . "?" . $querys;
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_FAILONERROR, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    if (1 == strpos("$".$host, "https://"))
+    {
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    }
+    $d=json_decode(curl_exec($curl));
+  $tq=$d->showapi_res_body->list;
+  foreach($tq as $v){
+echo "<pre>";  
+var_dump($v->area);
+  }
+?>
---
+++ project/tianqi.php
@@ -0,0 +1,15 @@
+﻿  <!doctype html>
+  <html lang="en">
+  <head>
+  	<meta charset="UTF-8" />
+  	<title>精彩文件</title>
+  </head>
+  <body>
+  		<form action="tian.php" method="post" >
+		 <p>天气预报建设中</p>
+		<input type="text" name="name"  />
+		<input type="submit" value="提交" />
+       </form>
+  </body>
+  </html>
+  
